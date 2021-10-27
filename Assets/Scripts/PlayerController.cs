f28aa090aada94e4994965bb9a508e0e8b3d24a3
Francisco Lopez <franlopbri@gmail.com>
2021-10-26 19:12:02 -0500

    Script de Control y funciones - Jump

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //Variables del movimiento del personaje
+    public float jumpForce = 6f;
+    private Rigidbody2D rigidBody;
+
+    private void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
+            Jump();
+        }
+    }
+
+    void Jump()
+    {
+        rigidBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+    }
+}
