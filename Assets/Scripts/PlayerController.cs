7347d50e9bdd0cd53d75c51d26dda6af54ff26d2
franlops <franlopbri@gmail.com>
2022-09-19 13:00:36 -0500

    Programando funcion Jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,11 @@ public class PlayerController : MonoBehaviour
     public float jumpForce = 6f;
     Rigidbody2D rigidBody;
 
+    void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();    
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,11 +22,14 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
+            Jump();
+        }
     }
 
     void Jump()
     {
-
+        rigidBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
     }
 }
