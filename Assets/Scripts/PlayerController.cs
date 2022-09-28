6db414bbec8846f65f0dc8c16144ad4a20a9940f
franlops <franlopbri@gmail.com>
2022-09-28 15:09:51 -0500

    Cambia la animacion al saltar

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,53 +4,69 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Variables del movimiento del personaje
+
+    //Variables del movimiento del personaje
     public float jumpForce = 6f;
     Rigidbody2D rigidBody;
+    Animator animator;
+
+    const string STATE_ALIVE = "isAlive";
+    const string STATE_ON_THE_GROUND = "isOnTheGround";
 
     public LayerMask groundMask;
 
     void Awake()
     {
-        rigidBody = GetComponent<Rigidbody2D>();    
+        rigidBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
-    // Start is called before the first frame update
+    // Use this for initialization
     void Start()
     {
-        
+        animator.SetBool(STATE_ALIVE, true);
+        animator.SetBool(STATE_ON_THE_GROUND, true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
         {
             Jump();
         }
-        Debug.DrawRay(this.transform.position, Vector2.down * 1.4f, Color.red);
+
+        animator.SetBool(STATE_ON_THE_GROUND, IsTouchingTheGround());
+
+        Debug.DrawRay(this.transform.position, Vector2.down * 1.5f, Color.red);
     }
 
     void Jump()
     {
-        if (IsTouchingTheGround()) 
+        if (IsTouchingTheGround())
         {
             rigidBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         }
     }
 
-    //Nos indica si el personaje está tocando el suelo
+    //Nos indica si el personaje está o no tocando el suelo
     bool IsTouchingTheGround()
     {
-        if(Physics2D.Raycast(this.transform.position,
-                            Vector2.down, 1.4f, groundMask))
+        if (Physics2D.Raycast(this.transform.position,
+                             Vector2.down,
+                             1.5f,
+                             groundMask))
         {
-            //TODO: Programar lógica de contacto con el suelo
+            //TODO: programar lógica de contacto con el suelo
+            animator.enabled = true;
             return true;
-        } else
+        }
+        else
         {
-            //TODO: Programar lógica de no contacto
-            return false; 
+            //TODO: programar lógica de no contacto
+            animator.enabled = false;
+            return false;
         }
     }
+
 }
